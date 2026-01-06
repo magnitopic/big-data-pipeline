365b9e98b8c2a77291681c6d45cb532c9bb723dc
Hugo <hugoagreda22@gmail.com>
2026-01-06 23:11:36 +0100

    finale

--- docs/Reset total.md
+++ docs/Reset total.md
@@ -26,23 +26,31 @@ Si no existe:
 
 kafka-topics --bootstrap-server kafka:9092 --create --topic opensky.usa.raw --partitions 1 --replication-factor 1
 
+kafka-topics --bootstrap-server kafka:9092 --create --topic openweather.usa.raw --partitions 1 --replication-factor 1
+
 **3. Proton – Activar VPN**
 
 **3. NiFi – Arrancar flujos**
 
 **4. Spark – Arrancar .py**
 
-docker exec -it spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 /opt/spark-apps/pysparkStreamingConnector.py
+docker exec -it spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --executor-cores 6 --total-executor-cores 6 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 /opt/spark-apps/flights_streaming.py
+
+docker exec -it spark-master /opt/spark/bin/spark-submit --master spark://spark-master:7077 --executor-cores 6 --total-executor-cores 6 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 /opt/spark-apps/weather_streaming.py
 
 **5. Cassandra – Comprobar base de datos**
 
 docker exec -it cassandra cqlsh
 
-USE flight\_weather\_streaming;
+USE flight_weather_streaming;
 
-SELECT COUNT(\*) FROM flights\_rt;  
+SELECT COUNT(*) FROM flights_rt;  
 
-SELECT \* FROM flights\_rt LIMIT 5;
+SELECT * FROM flights_rt LIMIT 5;
+
+SELECT COUNT(*) FROM weather_rt;
+
+SELECT * FROM weather_rt LIMIT 5;
 
 Debe incrementar cada minuto
 
--- src/flights_streaming.py
+++ src/flights_streaming.py
@@ -1,63 +1,41 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json, explode, current_timestamp, from_unixtime, to_timestamp
+from pyspark.sql.functions import col, from_json, explode, from_unixtime, to_timestamp
 from pyspark.sql.types import *
 import os
 
-# ========================
-# CONFIG
-# ========================
 CASSANDRA_HOST = os.getenv("CASSANDRA_HOST", "cassandra")
-CASSANDRA_PORT = os.getenv("CASSANDRA_PORT", "9042")
 CASSANDRA_KEYSPACE = os.getenv("CASSANDRA_KEYSPACE_STREAMING", "flight_weather_streaming")
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
 FLIGHTS_TOPIC = os.getenv("FLIGHTS_KAFKA_TOPIC", "opensky.usa.raw")
 
-# ========================
-# SPARK SESSION
-# ========================
 spark = (
     SparkSession.builder
-    .appName("OpenSky-USA-Streaming")
+    .appName("Flights-USA-Streaming")
     .config("spark.cassandra.connection.host", CASSANDRA_HOST)
-    .config("spark.cassandra.connection.port", CASSANDRA_PORT)
     .getOrCreate()
 )
 
 spark.sparkContext.setLogLevel("WARN")
 
-# ========================
-# OPEN SKY SCHEMA
-# ========================
 schema = StructType([
     StructField("time", LongType()),
     StructField("states", ArrayType(ArrayType(StringType())))
 ])
 
-# ========================
-# KAFKA STREAM
-# ========================
 raw = (
     spark.readStream
     .format("kafka")
     .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)
     .option("subscribe", FLIGHTS_TOPIC)
     .option("startingOffsets", "latest")
-    .option("maxOffsetsPerTrigger", 200)
-    .option("failOnDataLoss", "false")
     .load()
 )
 
-
 json_df = raw.selectExpr("CAST(value AS STRING) as json")
-
 parsed = json_df.select(from_json(col("json"), schema).alias("data"))
-
 states = parsed.select(explode(col("data.states")).alias("s"))
 
-# ========================
-# TRANSFORM TO CASSANDRA MODEL
-# ========================
 clean = states.select(
     col("s")[0].alias("icao24"),
     to_timestamp(from_unixtime(col("s")[3].cast("long"))).alias("time_position"),
@@ -75,9 +53,6 @@ clean = states.select(
     col("time_position").isNotNull()
 )
 
-# ========================
-# WRITE TO CASSANDRA
-# ========================
 query = (
     clean.writeStream
     .trigger(processingTime="15 seconds")
@@ -87,6 +62,6 @@ query = (
     .option("checkpointLocation", "/opt/spark-data/checkpoints/opensky")
     .outputMode("append")
     .start()
-    )
+)
 
 query.awaitTermination()
